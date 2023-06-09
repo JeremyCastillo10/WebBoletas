74e3da07491e84bb5a61c6897cf68718261afb91
Jey <jeremycastillo901@gmail.com>
2023-06-09 16:41:08 -0400

    other

--- ProjectVenta/src/App.jsx
+++ ProjectVenta/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import './Components/Navbar'
+import Navbar from './Components/Navbar'
+import ProductSection from './Components/ProductSection'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+ <div className="App">
+  <Navbar></Navbar>
+  <ProductSection></ProductSection>
+  </div>
   )
 }
 
